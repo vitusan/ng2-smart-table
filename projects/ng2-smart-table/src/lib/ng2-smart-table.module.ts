6245d017f730e104cf2d9c46bd2ae4a1c0ff775b
Victor Andrade <0vetor0@gmail.com>
2023-09-06 15:40:23 -0300

    removing nebular

--- projects/ng2-smart-table/src/lib/ng2-smart-table.module.ts
+++ projects/ng2-smart-table/src/lib/ng2-smart-table.module.ts
@@ -9,7 +9,6 @@ import { TBodyModule } from './components/tbody/tbody.module';
 import { THeadModule } from './components/thead/thead.module';
 
 import { Ng2SmartTableComponent } from './ng2-smart-table.component';
-import { NbInputModule } from '@nebular/theme';
 
 @NgModule({
   imports: [
@@ -21,7 +20,6 @@ import { NbInputModule } from '@nebular/theme';
     PagerModule,
     TBodyModule,
     THeadModule,
-    NbInputModule,
   ],
   declarations: [
     Ng2SmartTableComponent,
